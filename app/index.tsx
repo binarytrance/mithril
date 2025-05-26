a0d258e27dfea664eed2968e6c524fde406b053b
binarytrance <ganeshan.dash@gmail.com>
2025-05-26 22:49:14 +0530

    rendering ShoppinglIstems dynamically from an array

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,28 @@ import { StyleSheet, View } from 'react-native';
 import { theme } from '../theme';
 import { ShoppingListItem } from '../Components/ShoppingListItem';
 import { Link } from 'expo-router';
+import { useState } from 'react';
+
+type ShoppingListItemType = {
+    id: string;
+    name: string;
+    isCompleted?: boolean;
+};
+
+const initialList: ShoppingListItemType[] = [
+    { id: '1', name: 'Coffee', isCompleted: false },
+    { id: '2', name: 'Tea', isCompleted: true },
+    { id: '3', name: 'Milk', isCompleted: false }
+];
 
 export default function App() {
+    const [shoppingList] = useState(initialList);
     return (
         <View style={styles.container}>
             <Link href='/counter'>Go to Counter</Link>
-            <ShoppingListItem name='Coffee' />
-            <ShoppingListItem name='Tea' isCompleted />
-            <ShoppingListItem name='Coke' />
+            {shoppingList.map(item => (
+                <ShoppingListItem name={item.name} key={item.id} isCompleted={item.isCompleted} />
+            ))}
         </View>
     );
 }
