6504a4a1775a7d697b7f0f67c714dcf2f992a226
binarytrance <ganeshan.dash@gmail.com>
2025-05-26 22:54:26 +0530

    added textinput and connected to list

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TextInput } from 'react-native';
 import { theme } from '../theme';
 import { ShoppingListItem } from '../Components/ShoppingListItem';
 import { Link } from 'expo-router';
@@ -17,10 +17,27 @@ const initialList: ShoppingListItemType[] = [
 ];
 
 export default function App() {
-    const [shoppingList] = useState(initialList);
+    const [shoppingList, setShoppingList] = useState(initialList);
+    const [value, setValue] = useState<string>();
     return (
         <View style={styles.container}>
             <Link href='/counter'>Go to Counter</Link>
+            <TextInput
+                value={value}
+                style={styles.textInput}
+                onChangeText={setValue}
+                placeholder='Coffee'
+                onSubmitEditing={() => {
+                    if (value) {
+                        setShoppingList(prev => [
+                            { id: Math.random().toString(), name: value, isCompleted: false },
+                            ...prev
+                        ]);
+                        setValue(undefined);
+                    }
+                }}
+                returnKeyType='done'
+            />
             {shoppingList.map(item => (
                 <ShoppingListItem name={item.name} key={item.id} isCompleted={item.isCompleted} />
             ))}
@@ -35,6 +52,16 @@ const styles = StyleSheet.create({
         // alignItems: 'center',
         justifyContent: 'center',
         paddingHorizontal: 8,
-        paddingVertical: 10
+        paddingVertical: 10,
+        paddingTop: 12
+    },
+    textInput: {
+        borderColor: theme.colorLightGrey,
+        borderWidth: 2,
+        padding: 12,
+        fontSize: 18,
+        borderRadius: 50,
+        marginHorizontal: 12,
+        marginBottom: 12
     }
 });
